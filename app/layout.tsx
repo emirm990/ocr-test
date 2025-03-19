1f91ac47a585fdc9bd962336d43231446302d625
Emir Mekić <emirmekic123@gmail.com>
2025-03-19 13:05:57 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,9 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    template: '%s | OCR',
+    default: 'OCR',
   },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
 export default function RootLayout({
   children,
